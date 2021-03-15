c47c1bb65d8f900fbeab6e7dc229fb1a0ae11a62
姜佳春 <jjccoke@gmail.com>
2021-03-15 10:27:49 +0800

    升级 spring boot

--- src/main/java/com/jjc/storage/FileSystemStorageService.java
+++ src/main/java/com/jjc/storage/FileSystemStorageService.java
@@ -19,6 +19,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
